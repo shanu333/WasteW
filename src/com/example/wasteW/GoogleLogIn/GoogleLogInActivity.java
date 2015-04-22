1e76765d1e479388871190f1f6f126dde7ec0693
Shantanu <shantanu@zophop.com>
2015-04-22 16:05:15 +0530

    adding google play serice

---
+++ src/com/example/wasteW/GoogleLogIn/GoogleLogInActivity.java
@@ -0,0 +1,14 @@
+package com.example.wasteW.GoogleLogIn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class GoogleLogInActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
